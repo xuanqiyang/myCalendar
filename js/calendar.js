dae561dd936324cdc1b4e42ed44843459243f15d
xuanqiYang <1354570828@qq.com>
2018-03-10 16:32:27 +0800

    填充上月和下月的天数到日历

--- js/calendar.js
+++ js/calendar.js
@@ -1,31 +1,36 @@
 var Calendar = function(ele) {
     var eleCalendar = document.querySelector(ele);
-    var Now = function() {
-        var date = new Date();
-        this.dayOnWeek = date.getDay(), //获取当日星期
-        this.month = date.getMonth() + 1, //获取当日月份
-        this.dayOnMonth = date.getDate(), //获取当日月份
-        this.year = date.getFullYear(), //获取当日年份
-        this.daysOnThisMonth = new Date(this.year, this.month, 0).getDate() //获取当月总天数
+    var _Date = function() {
+        var date;
+        if (arguments.length) { //如果自定义参数的话就设置日期
+            date = new Date(Array.prototype.slice.apply(arguments,[0,3]));
+        }else{
+            date = new Date();
+        }
+        this.dayOfWeek = date.getDay(); //获取当日星期
+        this.month = date.getMonth() + 1; //获取当日月份
+        this.dayOfMonth = date.getDate(); //获取当月日期
+        this.year = date.getFullYear(); //获取当日年份
+        this.daysInThisMonth = new Date(this.year, this.month, 0).getDate();//获取当月总天数
     };
+    var _now = new _Date(2018,4,4); // 设置日期
 
-    var _now = new Now();
     var weekIsfirstDayOfMonth = new Date(_now.year, _now.month - 1, 1).getDay(); //这个月第一天是星期几
-    var IndexOflastday = weekIsfirstDayOfMonth + _now.daysOnThisMonth //这个月最后一天的在日历表格中的索引
+    var IndexOflastday = weekIsfirstDayOfMonth + _now.daysInThisMonth - 1 //这个月最后一天的在日历表格中的索引
     var renderCalendar = function() {
-        var fragCalendar = document.createDocumentFragment();
-        var eleWeekdays = document.createElement('ul');
-        var eleMonthdays = document.createElement('ul');
-        var indexofDayOfMonth = weekIsfirstDayOfMonth;
-        var day = 1;
-        var dayOfNextMonth=1;
-        var dayOfPreMonth = new Date(_now.year, _now.month-2, 0).getDate() //获取上个月的天数
+        var dayInAMonth = 1,
+            dayOfNextMonth = 1,
+            dayOfPreMonth = new Date(_now.year, _now.month - 1, 0).getDate(), //获取上个月的天数
+            elMonthdays,
+            indexofDayOfMonth = weekIsfirstDayOfMonth-1,
+            weekdaysText = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'],
+            fragCalendar = document.createDocumentFragment(),
+            eleWeekdays = document.createElement('ul'),
+            eleMonthdays = document.createElement('ul');
 
-        eleWeekdays.className = 'weekdays';
-        eleMonthdays.className = 'monthdays';
-
-        var weekdaysText = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-
+            eleWeekdays.className = 'weekdays';
+            eleMonthdays.className = 'monthdays';
+            
         // 渲染日历UI
         for (var i = 0; i < weekdaysText.length; i++) {
             var liWeekday = document.createElement('li');
@@ -43,21 +48,20 @@ var Calendar = function(ele) {
         fragCalendar.appendChild(eleMonthdays);
         eleCalendar.appendChild(fragCalendar);
 
-        var elMonthdays= eleCalendar.querySelector('.monthdays');
-        console.log(elMonthdays.children.length)
+        var monthdays = eleCalendar.querySelector('.monthdays');
+
         // 填充日历中的天数
-        for(var liNumOfDays = 0; elMonthdays.children.length >= liNumOfDays; liNumOfDays++){
-            if(liNumOfDays >= weekIsfirstDayOfMonth && day <= _now.daysOnThisMonth){ //如果填充的格子在某一个月之内
-                eleMonthdays.children[liNumOfDays].innerText = day++;
-            }else if(liNumOfDays >= IndexOflastday && day <= elMonthdays.children.length){ //如果这个月的天数没有填满日历表格,填充下个月的格子
-                eleMonthdays.children[liNumOfDays].innerText = dayOfNextMonth++;
-            }else if(liNumOfDays < weekIsfirstDayOfMonth) {
-                eleMonthdays.children[liNumOfDays].innerText=dayOfPreMonth--;
+        for (var liNum = 0; liNum <= monthdays.children.length ; liNum++) {
+            if (liNum >= weekIsfirstDayOfMonth && dayInAMonth <= _now.daysInThisMonth) { //如果填充的格子在某一个月之内
+                monthdays.children[liNum].innerText = dayInAMonth++;
+
+            } else if (liNum > IndexOflastday && liNum < monthdays.children.length) { //如果这个月的天数没有填满日历表格,填充下个月的格子
+                monthdays.children[liNum].innerText = dayOfNextMonth++;
+
+            } else if (liNum < weekIsfirstDayOfMonth && indexofDayOfMonth>=0) { //补充上个月的格子
+                monthdays.children[liNum].innerText = dayOfPreMonth-indexofDayOfMonth;
+                indexofDayOfMonth--;
             }
         }
     }();
-    for (var i = weekIsfirstDayOfMonth; i <= _now.daysOnThisMonth; i++) {
-
-    }
-    console.log(_now.daysOnThisMonth);
 };
